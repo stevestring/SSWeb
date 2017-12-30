c74db42f05021408771fd11e417dafe6d22346ae
stevestring <stevestring@gmail.com>
2017-12-30 15:52:12 -0500

    Added Nuget Package

--- SSWeb/Managers/EmailManager.cs
+++ SSWeb/Managers/EmailManager.cs
@@ -5,42 +5,12 @@ using System.Web.Security;
 using System.Net;
 using SendGrid;
 using SendGrid.Helpers.Mail;
-
+using System.Configuration;
 namespace SSWeb.Managers
 {
     public class EmailManager
     {
         private const string EmailFrom = "noreply@360cells.com";
-        public static void SendConfirmationEmailOld(string userName)
-        {
-            var user = Membership.GetUser(userName.ToString());
-            var confirmationGuid = user.ProviderUserKey.ToString();
-            var verifyUrl = HttpContext.Current.Request.Url.GetLeftPart
-               (UriPartial.Authority) + "/Account/Verify/" + confirmationGuid;
-
-            using (var client = new SmtpClient("smtp.sendgrid.net"))
-            {
-                using (var message = new MailMessage(EmailFrom, user.Email))
-                {
-                    message.Subject = "Please Verify your Account";
-                    message.Body = "<html><head><meta content=\"text/html; charset=utf-8\" /></head><body><p>Dear " + user.UserName +
-                        ", </p><p>To verify your account, please click the following link:</p>"
-                        + "<p><a href=\"" + verifyUrl + "\" target=\"_blank\">" + verifyUrl
-                        + "</a></p><div>Best regards,</div><div>360cells.com</div><p>Do not forward "
-                        + "this email. The verify link is private.</p></body></html>";
-
-                    message.IsBodyHtml = true;
-
-                    NetworkCredential Credentials = new NetworkCredential("360cells",
-                        "webconfig");
-                    client.Credentials = Credentials;
-                    //client.Port = 587;
-                    client.Port = 25;
-                    client.Send(message);
-                };
-            };
-        }
-
 
 
         public static void SendConfirmationEmail(string userName)
@@ -50,10 +20,10 @@ namespace SSWeb.Managers
             var verifyUrl = HttpContext.Current.Request.Url.GetLeftPart
                (UriPartial.Authority) + "/Account/Verify/" + confirmationGuid;
 
-            
+
 
             // Retrieve the API key from the environment variables. See the project README for more info about setting this up.
-            var apiKey = "webconfig";
+            var apiKey = ConfigurationManager.AppSettings["SendGridKey"];
 
             var client = new SendGridClient(apiKey);
 
